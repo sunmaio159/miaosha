995ea740ce3f000fabb39f64f7dfb062392196b2
sunmiao <sunmiao@qq.com>
2019-10-29 09:35:25 +0800

    第一次提交

---
+++ src/main/java/com/miaoshaproject/miaosha/MiaoshaApplication.java
@@ -0,0 +1,13 @@
+package com.miaoshaproject.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class MiaoshaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoshaApplication.class, args);
+    }
+}
---
+++ src/main/java/com/miaoshaproject/miaosha/TestController.java
@@ -0,0 +1,12 @@
+package com.miaoshaproject.miaosha;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello world";
+    }
+}
